326bc2428b76b3a4acadafe723c43bb3cd94e357
Arvind <arvindkhadri@gmail.com>
2015-05-13 17:24:10 +0530

    Modify runapp.py
    Modify runapp.py so that it can be also used as a launcher for wsgi.

--- servers/runapp.py
+++ servers/runapp.py
@@ -1,4 +1,14 @@
-from emule import app
+import os
+import sys
+
+
+# Get the path to the base directory of the app
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+# append the path to the WSGI env path
+sys.path.insert(0, BASE_DIR)
+
+from emule import app as application
 
 if __name__ == "__main__":
-    app.run('localhost', 5000, True)
+    application.run('localhost', 5000, True)
