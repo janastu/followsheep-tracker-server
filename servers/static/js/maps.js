14064821e4cd5c9e83b7ae89bec6b248ea26c30f
Arvind <arvindkhadri@gmail.com>
2015-05-13 17:30:33 +0530

    Change coordinates in map
    Load the map with Bangalore in viewport.

--- servers/static/js/maps.js
+++ servers/static/js/maps.js
@@ -1,6 +1,6 @@
 window.App = window.App || {};
 (function(L, App) {
-  App.map = new L.map("map").setView([51.505, -0.09], 13);
+  App.map = new L.map("map").setView([12.9791703, 77.5912429], 13);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(App.map);
