cbbab9f42cbb2116335f399f741db6ff799d253b
salus-sage <salus.sage@gmail.com>
2015-08-31 19:32:51 +0530

    Bug fix: Upload Browse accept only .zip Documentation button in nav bar misbehave

--- servers/static/js/maps.js
+++ servers/static/js/maps.js
@@ -4,15 +4,7 @@ window.App = window.App || {};
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(App.map);
-    App.mapIcon = new L.Icon({
-        iconUrl: '/static/images/leaf-green.png',
-        iconAnchor: [13, 30],
-        iconSize: [25, 41],
-        popupAnchor:[-2, -20],
-        shadowSize: [41, 41]
-    });
-    L.marker([512.9791, 77.59124], {icon: App.mapIcon}).addTo(App.map);
-  //side menu slide/toggle
+  //side menu slide toggle
   $(".menu-toggle").click(function(e) {
       e.preventDefault();
       $(".wrapper").toggleClass("active");
