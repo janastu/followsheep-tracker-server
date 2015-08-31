00d400069caac79064a3c37d1436aeecb98e9381
Arvind <arvindkhadri@gmail.com>
2015-08-31 15:58:52 +0530

    Fix shell script
    Check if avconv is installed, use it. If avconv is not installed then
    use ffmpeg

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,6 +1,13 @@
 # !/usr/bin/env bash
 cd $1
-for i in *.3gpp ; do
-    ffmpeg -i "$i" -codec:a libmp3lame -qscale:a 2 $(basename $i).mp3 > /dev/null & 2> /dev/null
-    echo "converted $i"
-done
+if command -v avconv 2> /dev/null; then
+    for i in *.3gpp ; do
+        avconv -i "$i" -codec:a libmp3lame -qscale:a 2 $(basename $i).mp3 > /dev/null & 2> /dev/null
+        echo "converted $i"
+    done
+else
+    for i in *.3gpp ; do
+        ffmpeg -i "$i" -codec:a libmp3lame -qscale:a 2 $(basename $i).mp3 > /dev/null & 2> /dev/null
+        echo "converted $i"
+    done
+fi
